b71df1aaac32e207d7a311a4c6eff1bd1fd31c7c
WinverLemon <787942271@qq.com>
2022-04-19 20:40:52 +0800

    更新 m_jd_follow_shop.js

--- m_jd_follow_shop.js
+++ m_jd_follow_shop.js
@@ -10,7 +10,7 @@ cron:10 10 10 10 *
 */
 let mode = __dirname.includes('magic')
 const {Env} = mode ? require('./magic') : require('./magic')
-const $ = new Env('自动车-M关注有礼');
+const $ = new Env('M关注有礼');
 $.followShopArgv = process.env.M_FOLLOW_SHOP_ARGV
     ? process.env.M_FOLLOW_SHOP_ARGV
     : '';
